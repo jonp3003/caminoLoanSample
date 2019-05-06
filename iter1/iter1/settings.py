3789c02b24abf006389369ec8a7139dbcbc4467e
jonp3003 <50134158+jonp3003@users.noreply.github.com>
2019-05-05 19:35:44 -0700

    Update settings.py

--- iter1/iter1/settings.py
+++ iter1/iter1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'camfintest1.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'peaceful-fjord-99949.herokuapp.com']
 
 
 # Application definition
